db11ae612cd7d402d1ea077728509bda05b19bdd
陳信屹 <ossug.hychen@gmail.com>
2015-05-29 19:59:21 +0800

    include testcheck types in test.d.ts.

--- typings/test/tsd.d.ts
+++ typings/test/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="chai/chai.d.ts" />
+/// <reference path='node_modules/testcheck/dist/testcheck.d.ts'/>
 /// <reference path="mocha-testcheck.d.ts" />
