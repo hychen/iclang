335e89b3d82d9ec0dc15ed258d76c651a9344881
陳信屹 <ossug.hychen@gmail.com>
2015-06-01 20:43:44 +0800

    extract rpcSocketAddr to rpc.ts.

--- src/lib/process.ts
+++ src/lib/process.ts
@@ -14,16 +14,6 @@ export enum ProcessStatus {
     terminating
 };
 
-/** Takes a process name and returns a ipc address.
- * @param {string}  processName - a process name.
- * @returns {string} ipc address.
- */
- export function rpcSocketAddr(processName: string): string {
-    var runtimeDir = process.env.RUNTIME_ROOT_DIR || './.ic';
-    var fname = 'ipc-process-' + processName;
-    return path.join(runtimeDir, 'socket', fname);
- }
-
 interface ExitCallbacks {
     [key: string]: (any) => any;
 }
---
+++ src/lib/rpc.ts
@@ -0,0 +1,15 @@
+/** Module RPC
+ *
+ * Copyright (c) 2015 Chen Hsin-Yi
+ * MIT License, see LICENSE file for full terms.
+ */
+
+/** Takes a process name and returns a ipc address.
+ * @param {string}  processName - a process name.
+ * @returns {string} ipc address.
+ */
+ export function rpcSocketAddr(processName: string): string {
+    var runtimeDir = process.env.RUNTIME_ROOT_DIR || './.ic';
+    var fname = 'ipc-process-' + processName;
+    return path.join(runtimeDir, 'socket', fname);
+ }
