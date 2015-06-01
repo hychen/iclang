7eb81a0f059b9973687d8ad65431f048539b4531
陳信屹 <ossug.hychen@gmail.com>
2015-06-01 22:21:02 +0800

    Ad some description of BaseComponent.

--- src/lib/component.ts
+++ src/lib/component.ts
@@ -8,8 +8,28 @@ export interface ExitCallbacks {
 }
 
 interface BaseComponent {
+    /* A display name for the component.
+     * - Sentence-case (like a normal sentence)
+     * - No ending punctuation.
+     */
     friendlyName: string;
+    /* Clear, 1 sentence description in the imperative mood.
+     */
     description?: string;
+    /* Provides supplemental info on top of description.
+     */
+    extendedDescription?: string;
+    /**
+     * This optional URL points to somewhere where additional information
+     * about the underlying functionality in this component can be found.
+     *
+     * Be sure and use a fully qualified URL like http://foo.com/bar/baz.
+     */
+    moreInfoUrl?: string;
+    // Can this component be cached?
+    cacheable?: boolean;
+    // Sync?
+    sync?: boolean;
 }
 
 export interface ActComponent extends BaseComponent {
