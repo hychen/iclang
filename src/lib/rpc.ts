85f698b887e098ebb150b851b613b8412e8a4554
陳信屹 <ossug.hychen@gmail.com>
2015-06-08 14:28:00 +0800

    Add rpcCallbackPingMethod.

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -4,13 +4,22 @@
  * MIT License, see LICENSE file for full terms.
  */
 import path = require('path');
+import zerorpc = require('zerorpc');
+import PS = require('./process');
+import C = require('./component');
 
 /** Takes a process name and returns a ipc address.
  * @param {string}  processName - a process name.
  * @returns {string} ipc address.
  */
- export function rpcSocketAddr(processName: string): string {
+export function rpcSocketAddr(processName: string): string {
     var runtimeDir = process.env.RUNTIME_ROOT_DIR || './.ic';
     var fname = 'ipc-process-' + processName;
     return path.join(runtimeDir, 'socket', fname);
- }
+}
+
+export function rpcProcessPingMethod(unknown: any, reply: any) {
+    reply(null, 'pong');
+}
+
+
---
+++ src/test/unit/test_rpc.ts
@@ -0,0 +1,67 @@
+/// <reference path="../bootstrap-test.d.ts" />
+import mkdirp = require('mkdirp');
+import rimraf = require('rimraf');
+import RPC  = require('../../lib/rpc');
+import PS  = require('../../lib/process');
+
+function newActComp(){
+    return {
+        friendlyName: 'ActComp',
+        fn: () => true
+    }
+};
+
+function newSourceActComponent(){
+    return {
+        friendlyName: 'SourceActComp',
+        exits: {
+            success: {
+                description: 'returns 1.'
+            }
+        },
+        fn: (inputs, exits) => exits.success(1)
+    }
+}
+
+function newDestActComponent(done?){
+    return {
+        friendlyName: 'DestActComp',
+        inputs: {
+            in: {
+                description: 'an input value.'
+            }
+        },
+        fn: (inputs) => {
+            var res = inputs.in + 1;
+            if(done){
+                done(res);
+            }else{
+                return res;
+            }
+        }
+    }
+}
+
+describe('Process RPC Callbacks', () => {
+    beforeEach((done) => {
+        mkdirp(global['TEST_RUNTIME_ROOT_DIR'], (err) => {
+            if(err) throw err;
+            mkdirp(global['TEST_RUNTIME_SOCKET_DIR'], done);
+        });
+    });
+    afterEach((done) => {
+        rimraf(global['TEST_RUNTIME_ROOT_DIR'], () => {
+            rimraf(global['TEST_RUNTIME_SOCKET_DIR'], done);
+        });
+    });
+    describe('rpcCallbackPingMethod()', () => {
+        it('response `pong`.', (done) => {
+            var process = new PS.Process('A', newActComp());
+            var cb = RPC.rpcProcessPingMethod.bind(process);
+            cb(null, (err, res, more) => {
+                expect(res).to.eq('pong');
+                done();
+            });
+        });
+    });
+});
